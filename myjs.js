619673a45847f0cd2b41355492ee56011008b380
sidratchy <sidrathchy17@gmail.com>
2021-01-27 12:31:42 -0800

    changes

--- myjs.js
+++ myjs.js
@@ -25,6 +25,37 @@ totalSpan('totalBalance',depositNumber);
     
     
 });
+
+//witwhdraw event handler....
+const withdrawBtn = document.getElementById('withdrawBtn');
+withdrawBtn.addEventListener('click',function(){
+  const  withdrawAmount =  document.getElementById('withdrawAmount').value;
+  const  withdrawAmountNumber = parseFloat(withdrawAmount);
+  const  withdrawBalance = document.getElementById('withdrawBalance').innerText;
+  const withdrawNumber = parseFloat(withdrawBalance);
+  const totalWithdrawAmount =  withdrawAmountNumber + withdrawNumber;
+  
+  document.getElementById('withdrawBalance').innerText = totalWithdrawAmount;
+  document.getElementById('withdrawAmount').value = "";
+  
+  totalwithdraw('totalBalance', totalWithdrawAmount);
+
+  
+});
+function totalwithdraw(id,totalWithdrawAmount){
+    const totalDeposit = document.getElementById(id).innerText;
+    const totalDepositNumber = parseFloat(totalDeposit);
+    
+    const total = totalDepositNumber - totalWithdrawAmount;
+    if(total < 0){
+        alert('You have no sufficient Balance');
+    }else{
+    document.getElementById(id).innerText = total;
+    }
+}
+
+
+
 function totalSpan(id,depositNumber){
     const totalDeposit = document.getElementById(id).innerText;
     const totalDepositNumber = parseFloat(totalDeposit);
